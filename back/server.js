c38c5449ed59e7b1ff03a3c5dd93d6b518378d92
CaroTrd <carolinatirado92@gmail.com>
2019-11-02 13:44:08 +0100

    Add initial back(server) and front(reactjs)

---
+++ back/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+var cors = require("cors");
+const app = express();
+var server = require('http').createServer(app);
+const PORT = 8082;
+
+app.use(cors());
+
+app.use("/", function(req, res) {
+  res.setHeader("Content-Type", "text/plain");
+  res.send("Vous êtes à l'accueil");
+});
+
+server.listen(PORT, "localhost", function() {
+  console.log(
+    `Listening on port ${server.address().port}: ${server.address().address}`
+  );
+});
