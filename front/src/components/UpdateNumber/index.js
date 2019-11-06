e47c49063b70ec7eb278d170642d769c5079856a
CaroTrd <carolinatirado92@gmail.com>
2019-11-06 19:19:32 +0100

    create the component UpdateNumber

--- front/src/components/UpdateNumber/index.js
+++ front/src/components/UpdateNumber/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
+import { connect } from "react-redux";
 
 class FormUpdateNumber extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      dataContact: this.props.contacts,
       contact: {
         firstname: "",
         lastName: "",
@@ -20,7 +22,7 @@ class FormUpdateNumber extends Component {
 
   handleFirstnameChange = e => {
     // regex to check the input format of the user line 37, line 62 and line 87
-    const { contact, errorContact, boolContact } = this.state;
+    const { contact, errorContact } = this.state;
     this.setState({ contact: { ...contact, firstname: e.target.value } });
     if (e.target.value.trim() === "") {
       this.setState({
@@ -29,7 +31,6 @@ class FormUpdateNumber extends Component {
           errorFirstname: "Veuillez renseigner votre prénom"
         }
       });
-      this.setState({ boolContact: { ...boolContact, boolFirst: false } });
     } else if (!/^([a-zA-Zéèêëàâîïôöûü-].{2,150})$/gi.test(e.target.value)) {
       this.setState({
         errorContact: {
@@ -37,15 +38,13 @@ class FormUpdateNumber extends Component {
           errorFirstname: "Veuillez rentrer un prénom valide"
         }
       });
-      this.setState({ boolContact: { ...boolContact, boolFirst: false } });
     } else {
       this.setState({ errorContact: { ...errorContact, errorFirstname: "" } });
-      this.setState({ boolContact: { ...boolContact, boolFirst: true } });
     }
   };
 
   handleLastNameChange = e => {
-    const { contact, errorContact, boolContact } = this.state;
+    const { contact, errorContact } = this.state;
     this.setState({ contact: { ...contact, lastName: e.target.value } });
     if (e.target.value.trim() === "") {
       this.setState({
@@ -54,7 +53,6 @@ class FormUpdateNumber extends Component {
           errorLastName: "Veuillez renseigner votre nom de famille"
         }
       });
-      this.setState({ boolContact: { ...boolContact, boolLast: false } });
     } else if (!/^([a-zA-Zéèêëàâîïôöûü-].{2,150})$/gi.test(e.target.value)) {
       this.setState({
         errorContact: {
@@ -62,15 +60,13 @@ class FormUpdateNumber extends Component {
           errorLastName: "Veuillez rentrer un nom valide"
         }
       });
-      this.setState({ boolContact: { ...boolContact, boolLast: false } });
     } else {
       this.setState({ errorContact: { ...errorContact, errorLastName: "" } });
-      this.setState({ boolContact: { ...boolContact, boolLast: true } });
     }
   };
 
   handleTelNumberChange = e => {
-    const { contact, errorContact, boolContact } = this.state;
+    const { contact, errorContact } = this.state;
     this.setState({ contact: { ...contact, phone: e.target.value } });
     if (e.target.value.trim() === "") {
       this.setState({
@@ -79,7 +75,6 @@ class FormUpdateNumber extends Component {
           errorPhone: "Veuillez renseigner votre numéro de téléphone"
         }
       });
-      this.setState({ boolContact: { ...boolContact, boolPhone: false } });
     } else if (
       !/^((\+)[0-9]{2,3}[ ][0-9]{2}[ ])[1-9]{6,}$/gi.test(e.target.value)
     ) {
@@ -89,10 +84,8 @@ class FormUpdateNumber extends Component {
           errorPhone: "Veuillez respecter le format. Ex: +39 02 xxxxxxx"
         }
       });
-      this.setState({ boolContact: { ...boolContact, boolPhone: false } });
     } else {
       this.setState({ errorContact: { ...errorContact, errorPhone: "" } });
-      this.setState({ boolContact: { ...boolContact, boolPhone: true } });
     }
   };
 
@@ -104,7 +97,7 @@ class FormUpdateNumber extends Component {
       }),
       body: JSON.stringify({
         contact_id: id,
-        contact: contact
+        contact: this.state.contact
       })
     }).then(res => {
       if (res.status === 200) {
@@ -117,48 +110,59 @@ class FormUpdateNumber extends Component {
     });
   };
   render() {
-    const { errorContact } = this.state;
-    return (
-      <div>
-        {contacts.map((person, index) => {
-          const id = Number(props.match.params.id);
-          if (person.contact_id === id) {
-            return (
-              <ul key={index || person.contact_id}>
-                <li>
-                  <input
-                    type="tel"
-                    placeholder={person.firstname}
-                    onChange={e => handleFirstnameChange(e)}
-                  />
-                  {errorContact.errorFirstname}
-                </li>
-                <li>
-                  <input
-                    type="text"
-                    placeholder={person.last_name}
-                    onChange={e => handleLastNameChange(e)}
-                  />
-                  {errorContact.errorLastName}
-                </li>
-                <li>
-                  <input
-                    type="tel"
-                    placeholder={person.phone}
-                    onChange={e => handleTelNumberChange(e)}
-                  />
-                  {errorContact.errorPhone}
-                </li>
-                <button onClick={() => handleSubmit(person.contact_id)}>
-                  Click
-                </button>
-              </ul>
-            );
-          }
-        })}
-      </div>
-    );
+    const { dataContact, errorContact } = this.state;
+    const id = Number(this.props.match.params.id);
+    // filter the id taked in the url with the contact_id of the data to return the information about the number or name selected
+    const displayNumber = dataContact
+      .filter(select => {
+        return select.contact_id === id;
+      })
+      .map((person, index) => {
+        return (
+          <ul key={index || person.contact_id}>
+            <li>
+              <input
+                type="tel"
+                placeholder={person.firstname}
+                onChange={e => this.handleFirstnameChange(e)}
+              />
+              {errorContact.errorFirstname}
+            </li>
+            <li>
+              <input
+                type="text"
+                placeholder={person.last_name}
+                onChange={e => this.handleLastNameChange(e)}
+              />
+              {errorContact.errorLastName}
+            </li>
+            <li>
+              <input
+                type="tel"
+                placeholder={person.phone}
+                onChange={e => this.handleTelNumberChange(e)}
+              />
+              {errorContact.errorPhone}
+            </li>
+            <button onClick={() => this.handleSubmit(person.contact_id)}>
+              Click
+            </button>
+          </ul>
+        );
+      });
+    return <div>{displayNumber}</div>;
   }
 }
 
-export default withRouter(FormUpdateNumber);
+const mapStateToProps = state => {
+  return {
+    contacts: state.contacts
+  };
+};
+
+export default withRouter(
+  connect(
+    mapStateToProps,
+    null
+  )(FormUpdateNumber)
+);
